5091c35ed43e211d9bcd325bcb131cafd2e561e9
devgiants75 <devgiants75@naver.com>
2024-12-20 20:49:42 +0900

    1220 loop

--- python-241210/1220/B_practice.py
+++ python-241210/1220/B_practice.py
@@ -6,3 +6,47 @@
 # 3. 중첩 if-else문: 조건문 내부의 조건문
 # 4. elif문: 여러 조건을 순차적으로 확인
 # 5. 삼항 연산자: 조건문을 간결하게 표현
+
+# 1. 사용자로부터 입력 받은 숫자가 짝수인지 홀수인지 판별하는 프로그램
+# num 변수에 사용자로부터 숫자를 입력받기 : input
+# # 입력받은 숫자가 짝수인지 홀수인지 판별 : if-else
+
+num = int(input('숫자를 입력해주세요.'))
+
+if num % 2 == 0:
+    print('짝수입니다.')
+else:
+    print('홀수입니다.')
+
+# 2. 중첩 조건문을 이용한 성적 분류 프로그램
+
+score = int(input('성적을 입력하세요.(0 ~ 100): '))
+
+# 성적이 유효한 범위인지 확인
+
+if score < 0 or score > 100:
+    print('유효하지 않은 점수입니다.')
+else:
+    # 유효한 숫자만 입력 (0 ~ 100)
+    # 성적에 따라 등급 분류: 90점 이상(A), 80점 이상(B), 70점(C), 그외에는 F
+    if score >= 90:
+        grade = 'A'
+    elif score >= 80:
+        grade = 'B'
+    elif score >= 70:
+        grade = 'C'
+    else:
+        grade = 'F'
+    print(f'학생의 등급은 {grade}입니다.')
+
+# 딕셔너리를 사용한 항목 관리
+
+items = {'사과': 2000, '바나나': 1500, '체리': 900}
+
+item_name = '사과'
+
+# 딕셔너리 값 추출: 딕셔너리명[키명]
+if item_name in items:
+    print(f'{item_name}의 가격: {items[item_name]}')
+else:
+    print('해당 물품이 없습니다.')
---
+++ python-241210/1220/C_loop.py
@@ -0,0 +1,77 @@
+# C_loop
+
+# 파이썬 제어문 - 반복문(Loop)
+
+# 1. 반복문의 정의
+# : 특정 코드를 여러 번 실행하도록 프로그램에 지시하는 구조
+
+# 2. 반복문의 종류
+# : for 반복문, while 반복물
+
+# for 반복문
+# : 시퀀스 자료형을 순회하여 각 요소에 대해 특정 코드를 반복하도록 구현
+
+# cf) 시퀀스 자료형
+#       : 순서가 있는 자료형
+#       : 리스트, 튜플, 문자형
+
+'''
+for문의 기본 구조
+
+for 변수 in 시퀀스:
+    반복할 명령 (실행할 코드 블럭)
+'''
+
+numbers = [1, 2, 3, 4, 5]
+
+for num in numbers: # numbers 리스트에서 요소를 하나씩 꺼내 num에 담기
+    print(num) # for문의 실행문을 동작
+
+print('for문 종료')
+
+fruits = ['apple', 'banana', 'mango']
+for fruit in fruits:
+    print(fruit.upper()) # 문자열자료.upper(): 해당 문자열을 대문자로 변환
+
+# 특정 횟수만큼 반복
+# : range() 함수
+# - 연속된 숫자(정수)를 만들어주는 파이썬 내장 기능
+
+# 1) range(n): 0부터 n-1까지 정수를 생성
+for i in range(5):
+    print(i)
+
+# 2) range(n, m): n부터 m-1까지 정수를 생성
+for i in range(1, 4):
+    print(i)
+
+# 3) range(n, m, o): n부터 m-1까지 정수를 생성 (o는 각 숫자의 간격)
+for i in range(1, 10, 2):
+    print(i)
+
+# == 컬렉션 타입과의 for문 == #
+# : 리스트, 튜플, 딕셔너리
+numbers = [1, 2, 3, 4, 5] # 리스트
+for number in numbers:
+    print(number)
+
+numbers = (1, 2, 3, 4, 5) # 튜플
+for number in numbers:
+    print(number)
+
+# 딕셔너리 사용 for문
+
+# cf) 딕셔너리 활용 기능
+# 1) .items(): 딕셔너리의 모든 키-값 쌍을 반환하는 기능
+# 2) .keys(): 키를 모두 반환
+# 3) .values(): 값을 모두 반환
+
+fruitColors = {'apple': 'red', 'banana': 'yellow', 'cherry': 'red'}
+
+# === .items() 사용시 ===
+# 첫 번째 키를 fruit 변수에
+# , 두 번째 값을 color 변수에 할당하여 사용
+
+# cf) 딕셔너리명만 in 연산자에 사용하는 경우 key만을 반환
+for fruit, color in fruitColors.items():
+    print(f'The color of {fruit} is {color}')
