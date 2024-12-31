48c1f3e6eb38216de1f7a48660366c760e69f30a
devgiants75 <56944017+ahwaboin3@users.noreply.github.com>
2024-12-31 19:43:32 +0900

    1231 function practice

--- python-241210/1231/C_practice.py
+++ python-241210/1231/C_practice.py
@@ -11,5 +11,25 @@
 
 # ex) 3000원을 넣었을 때: 4잔의 음료수, 잔돈 200원
 
+def vending_machine(money):
+    # 자판기에 돈을 넣으면, 음료와 잔돈을 print()로 반환
 
+    # 음료수 가격
+    drink_price = 700
 
+    # 음료수 개수 & 잔돈 계산
+    # 계산 결과의 몫 - //
+    # 계산 결과의 나머지 - %
+
+    # 돈을 음료 가격으로 나눈 몫
+    num_drink = money // drink_price
+
+    # 돈을 음료 가격으로 나눈 나머지
+    change = money % drink_price
+
+    # 출력
+    print(f'{money}원을 넣었을 때: {num_drink}잔의 음료, 잔돈 {change}원')
+
+vending_machine(3000) # 3000원을 넣었을 때: 4잔의 음료, 잔돈 200원
+vending_machine(5000) # 5000원을 넣었을 때: 7잔의 음료, 잔돈 100원
+vending_machine(5600) # 5600원을 넣었을 때: 8잔의 음료, 잔돈 0원
